035050c5824367cde2b15a50f42e1e1a690f9888
Andrew McNutt <mcnutt.andrew@gmail.com>
2014-09-16 23:43:39 -0700

    On screen, randomly with correct length

--- app/assets/javascripts/views/entriesIndex.js
+++ app/assets/javascripts/views/entriesIndex.js
@@ -1,13 +1,14 @@
 Zoidberg.Views.entriesIndex = Backbone.View.extend({
-	template: JST["index"],
+	template: JST["entries/index"],
 	
 	initialize: function(){
 		this.listenTo(this.collection, "sync", this.render)
 	},
 	
 	render: function(){
-
-		var renderedContent = this.template({ collection: this.collection });
+		var renderedContent = this.template({
+			 collection: this.collection.sample(10) 
+		 });
 		this.$el.html(renderedContent);
 		return this;
 	}
